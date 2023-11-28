a0e2cce958e37ee237c3a72782b88f10d4e539ae
WILLIAM ELLIS <will@wearestudio77.com>
2023-11-28 15:15:34 -0600

    add serverless car api

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,26 @@
-const messageNode = document.getElementById('message')
-const render = (message="", node = messageNode)=>{
-    messageNode.textContent= message
+const messageNode = document.getElementById("message")
+
+const render = (cars = [], DOMNode = messageNode) => {
+    console.log(cars)
+    const frag = document.createDocumentFragment();
+    for (const car of cars) {
+        const li = document.createElement('li');
+        li.innerHTML = `<img src="${car.avatar_url}" /><span>${car.name} (${car.bhp})</span>`
+        frag.append(li)
+    }
+    DOMNode.append(frag)
 }
 
-const getData = async () =>{
-    try{
-        const response = await fetch('/.netlify/functions/hello-world')
+const getData = async () => {
+    try {
+        const response = await fetch('/.netlify/functions/read-all-cars')
         if(!response.ok) throw response
         const data = await response.json()
-        render(data.message)
-
-    }catch(err){
-
+        render(data)
+    } catch (err) {
+        alert("Error fetching. Check console")
+        console.error(err)
     }
 }
+
 getData()
