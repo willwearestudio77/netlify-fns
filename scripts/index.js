14d3146101d5834db84cc41db6a1c067245dd267
WILLIAM ELLIS <will@wearestudio77.com>
2023-11-28 14:42:45 -0600

    intergrate with serverless function

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,16 @@ const messageNode = document.getElementById('message')
 const render = (message="", node = messageNode)=>{
     messageNode.textContent= message
 }
-render("hello")
+
+const getData = async () =>{
+    try{
+        const response = await fetch('/.netlify/functions/hello-world')
+        if(!response.ok) throw response
+        const data = await response.json()
+        render(data.message)
+
+    }catch(err){
+
+    }
+}
+getData()
